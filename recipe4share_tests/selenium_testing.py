dccc7a3fd3f972bd93be7ab1cb32b8c2e18497cc
YenDuyenLeMavs <yenduyen.le@mavs.uta.edu>
2024-07-02 23:42:14 -0500

    added test cases

--- recipe4share_tests/selenium_testing.py
+++ recipe4share_tests/selenium_testing.py
@@ -90,25 +90,9 @@ class Recipe4ShareTests(unittest.TestCase):
         error_message = WebDriverWait(driver, 1).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "div.ui.error.message"))
         )     
+        time.sleep(5)
+
         assert error_message.is_displayed(), "Error message is not visible after login with invalid credentials"
-
-        driver.quit()
-
-    ## Verify error message appears after login with invalid credentials
-    def test_TC002(self):
-
-        driver = self.driver        
-
-        # Login with valid credentials
-        wrong_password = self.password + 'L'
-        login(driver, self.email, wrong_password)
-
-        # Wait up to 1 second to check that error message is visible after login with invalid credentials
-        error_message = WebDriverWait(driver, 1).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "div.ui.error.message"))
-        )     
-        assert error_message.is_displayed(), "Error message is not visible after login with invalid credentials"
-
         driver.quit()
 
     ## Verify registration with new username and new email
@@ -140,7 +124,7 @@ class Recipe4ShareTests(unittest.TestCase):
         )
 
         # Enter new username
-        username = str(int(time.time() * 1000))
+        username = str(int(time.time() * 1000)) # Ensure uniqueness
         username_field = driver.find_element(By.XPATH, "//input[@placeholder='Enter your username' and @type='text']")
         username_field.send_keys(username)
 
@@ -161,20 +145,88 @@ class Recipe4ShareTests(unittest.TestCase):
         submit_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Signup')]")
         submit_button.click()
 
-        # Wait up to 5 seconds for "Profile" button to appear after registration
-        profile_button = WebDriverWait(driver, 5).until(
-             EC.presence_of_element_located((By.XPATH, "//a[@href='/profile']"))
-        )
+        # Wait for toast message to go away so selenium can click profile button
+        time.sleep(8)
 
-        # Click on "Profile" button
+        # Wait up to 60 seconds to finish uploading new user then click on profile
+        profile_button = WebDriverWait(driver, 60).until(
+            EC.element_to_be_clickable((By.XPATH, "//a[@href='/profile']"))
+        )
         profile_button.click()
 
-        # Check presence of "Hi, <username>" to indicate new user automatically logged in after registration
-        greeting = driver.find_element(By.CLASS_NAME, "greetingContainer")
-        expected_greeting_text = "Hi, " + username
-        assert expected_greeting_text == greeting.text, f"Expected '{expected_greeting_text}' but found '{greeting.text}'"
+        # Construct expected greeting text
+        expected_greeting_text_username = f"Hi, {username}"
+        expected_greeting_text_email = f"Hi, {email}"  # Adjust this based on your logic
 
+        # Wait up to 1 second for greeting to be present to check if greeting matches newly registered user
+        greeting = WebDriverWait(driver, 1).until(
+             EC.presence_of_element_located((By.CLASS_NAME, "greetingContainer"))
+        )
+        actual_greeting_text = greeting.text.strip()
+        assert (actual_greeting_text == expected_greeting_text_username or 
+        actual_greeting_text == expected_greeting_text_email), \
+        f"Expected '{expected_greeting_text_username}' or '{expected_greeting_text_email}' but found '{actual_greeting_text}'"
+
+        time.sleep(2)
+        driver.quit()
+
+    ## Verify form submission for adding a recipe
+    def test_TC004(self):
+
+        driver = self.driver        
+
+        # Login with valid credentials
+        login(driver, self.email, self.password)
+
+        time.sleep(5)
+        
+        # Wait up to 5 seconds for "Add a recipe" button to be clickable after login with valid credentials
+        add_recipe_button = WebDriverWait(driver, 5).until(
+             EC.element_to_be_clickable((By.XPATH, '//a[@class="item"]/i[@class="plus icon"]'))
+        )   
+        add_recipe_button.click()
+
+        # Wait up to 5 seconds to for upload image input to be present to upload test image
+        upload_image = WebDriverWait(driver, 5).until(
+             EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="file"]'))
+        )   
+        upload_image.send_keys(os.path.abspath('./test_image.png'))
+        
+
+        # Enter title
+        title = str(int(time.time() * 1000)) # Ensure uniqueness
+        title_field = driver.find_element(By.XPATH, '//div[@class="field"]//label[text()="Title"]/following-sibling::div[@class="ui input"]/input[@type="text"]')
+        title_field.send_keys(title)
+
+        # Enter description
+        description = str(int(time.time() * 1000)) # Ensure uniqueness
+        description_field = driver.find_element(By.CSS_SELECTOR, 'textarea[placeholder="Recipe steps"]')
+        description_field.click()
+        description_field.send_keys(description)
+
+        # Click create recipe button
+        create_recipe = driver.find_element(By.XPATH, '//button[contains(@class, "ui icon primary right labeled button")]')
+        create_recipe.click()
+
+        # Wait for toast message to go away so selenium can click profile button
+        time.sleep(8)
+        
+        # Wait up to 5 seconds to check that "Profile" button is visible after login with valid credentials
+        profile_button = WebDriverWait(driver, 5).until(
+             EC.element_to_be_clickable((By.XPATH, "//a[@href='/profile']"))
+        )  
+        profile_button.click()
+
+        # Wait up to 30 seconds to check that new recipe is present
+        recipe_title = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, f'//div[@class="header" and contains(text(), "{title}")]'))
+        )
+        assert recipe_title.text == title, \
+            f"Expected '{title}' but found '{recipe_title.text}'"
+        time.sleep(20)
         driver.quit()
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
+
+
